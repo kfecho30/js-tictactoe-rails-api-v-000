9a74ce7d8dad727656a6c4282fb69b7a075e96cd
Kelsey Fecho <kfecho30@gmail.com>
2018-07-09 15:57:15 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,6 +1,10 @@
 // Code your JavaScript / jQuery solution here
 var turn = 0
-
+var winners = [
+  [0,1,2],[3,4,5],[6,7,8],
+  [0,4,8],[2,4,6],
+  [0,3,6],[1,4,7],[2,5,8]
+]
 $(document).ready(function(){
   attachListeners();
 });
@@ -36,4 +40,8 @@ function attachListeners() {
   $('#save').on('click', () => saveGame());
   $('#previous').on('click', () => previousGames());
   $('#clear').on('click', () => clearGame());
+
+  $('td').on('click', function(){
+    
+  });
 }
